0ada1e2f6227846030102beac35ad58ba555c0c6
YoutechA320U <42425276+YoutechA320U@users.noreply.github.com>
2024-07-16 22:52:58 +0900

    Update gradio-gguf-chat_karakuri.py

--- gradio-gguf-chat_karakuri.py
+++ gradio-gguf-chat_karakuri.py
@@ -54,8 +54,7 @@ def complement(role,prompt,turn_config):
                stop=["<|","prompt_tokens"] # ストップ。特定の文字を生成したらその文字を生成せず停止する。
         )
         output= output["choices"][0]["text"]
-        output =output.replace("\\n", "\n").replace("\\n", "\n").replace("\\u3000", "\u3000")\
-            .replace("!","！").replace("?","？").replace('\'choices\': [{\'text\': "','').replace('", \'index\': 0, \'logprobs\': None, \'finish_reason\': \'stop\'}], \'usage\': {\'',"")
+        output =output.replace("\\n", "\n").replace("\\u3000", "\u3000").replace("!","！").replace("?","？").replace('\'choices\': [{\'text\': "','').replace('", \'index\': 0, \'logprobs\': None, \'finish_reason\': \'stop\'}], \'usage\': {\'',"")
         while output[-1]=="\n":
               output=output[:-1]
         while output[0]=="\n":
